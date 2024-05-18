59b3416d0159fe5e5a29c05b151b751d1fe47016
aanis26 <anis777.rabehi@gmail.com>
2024-05-18 14:12:24 -0700

    TC01 generatekey

--- src/main/java/com/AesDEncrypt/Services/PasswordEncryptDecryptServiceImpl.java
+++ src/main/java/com/AesDEncrypt/Services/PasswordEncryptDecryptServiceImpl.java
@@ -1,4 +1,54 @@
 package com.AesDEncrypt.Services;
 
-public class PasswordEncryptDecryptServiceImpl {
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class PasswordEncryptDecryptServiceImpl implements PasswordEncryptDecryptService{
+
+    @Override
+    public String encrypt(String plainText, String secretKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        SecretKey key = new SecretKeySpec(secretKey.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+
+    }
+
+    @Override
+    public String decrypt(String encryptedText, String secretKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        SecretKey key = new SecretKeySpec(secretKey.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
+        return new String(decryptedBytes);
+    }
+
+    @Override
+    public String generateSecretKey(int keysize) throws NoSuchAlgorithmException {
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(keysize);
+        SecretKey secretKey = keyGen.generateKey();
+        byte[] keyBytes = secretKey.getEncoded();
+
+        return encode(keyBytes);
+
+
+    }
+
+
+
+
+   private String encode(byte[] data){
+        return  Base64.getEncoder().encodeToString(data);
+   }
+
+   private byte[] decode(String data){
+        return Base64.getDecoder().decode(data);
+   }
+
+
 }
